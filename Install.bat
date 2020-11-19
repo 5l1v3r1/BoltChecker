86f6d6a6e926bb8eaa609ef16b84947de835ad8e
Ignacio <wtf_ignacio@protonmail.com>
2020-11-19 05:49:37 +0000

    c

---
+++ Install.bat
@@ -0,0 +1,6 @@
+@echo off
+color 3
+title Boltchecker ^| Cracked by ignacio#2908
+cls
+python -m pip install -r requirements.txt
+pause
--- Installer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Boltchecker ^| Cracked by igna#6666
-cls
-pip install -r 	requirements.txt
-pause
-exit
